757907914a5fde5f9b63f02164ba788879ab5919
Hamd Waseem <hamd.waseem@hamdtel.co.uk>
2026-01-11 12:38:04 +0000

    feat: add Dockerfile and containerize renderer

--- renderer/renderer/main.py
+++ renderer/renderer/main.py
@@ -7,6 +7,7 @@ Main renderer
 import argparse
 from pydantic import ValidationError
 from renderer.schema import AnimationProject
+from pathlib import Path
 from renderer.render import render_project
 
 def render():
@@ -18,4 +19,12 @@ def render():
     parser.add_argument("--out", help="Optional output path for mp4", default=None)
     
     args = parser.parse_args()
+    
+    if args.out is None:
+        input_path = Path(args.path)
+        args.out = Path("/output") / f"{input_path.stem}.mp4"
+
     render_project(args.path, args.out)
+
+if __name__ == "__main__":
+    render()
